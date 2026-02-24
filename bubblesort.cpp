bca951e76814b65cfa9e0bacb52b7e7ebe7e8856
Rudrik <rudrik28novjoshi@gmail.com>
2026-02-25 00:39:44 +0530

    new changes

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main() {
 
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = 7;   // number of elements
+    int n = 7;   
 
-    // Bubble Sort
     for(int i = 0; i < n-1; i++) {
         for(int j = 0; j < n-i-1; j++) {
             if(arr[j] > arr[j+1]) {
